66bafa7655d478a3d4a076a7908d95cf7c475a8c
Nikk-0x11 <nikhildhiman9050@gmail.com>
2022-08-07 23:02:25 +0530

    updated merge sort

--- sorting/merge_sort.cpp
+++ sorting/merge_sort.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 void merge(int arr[], int p, int q, int r)
@@ -6,7 +7,10 @@ void merge(int arr[], int p, int q, int r)
   int n1 = q - p + 1;
   int n2 = r - q;
 
-  int L[n1], M[n2];
+  vector <int> L(n1);
+  vector <int> M(n2);
+
+  // int L[n1], M[n2];
 
   for (int i = 0; i < n1; i++)
     L[i] = arr[p + i];
